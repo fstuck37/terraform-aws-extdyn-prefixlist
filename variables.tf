b9d4245f49d7de2595f9f21779512c0c88021af0
Stuck <StuckF@dnb.com>
2021-09-02 23:19:26 -0400

    init

--- variables.tf
+++ variables.tf
@@ -31,12 +31,12 @@ variable "variables" {
 }
 
 variable "vpc_config" {
-  type = map(list(string))
+  type = map(map(list(string)))
   description = "Optional: A map of subnet_ids and security_group_ids"  
-  default = {
+  default = {{
     subnet_ids         = []
     security_group_ids = []
-  }
+  }}
 }
 
 /*
