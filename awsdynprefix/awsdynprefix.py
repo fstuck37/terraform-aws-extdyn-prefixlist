1af6eceba1dfc5253cde8d9c48874e15fa9b79c1
Stuck <StuckF@dnb.com>
2021-09-06 09:28:01 -0400

    init 3

--- awsdynprefix/awsdynprefix.py
+++ awsdynprefix/awsdynprefix.py
@@ -11,12 +11,12 @@ def logDictionary(dict):
 	try:
 		for key in dict:
 			logger.info('AWS Dynamic Prefix List Lambda - Info - logDictionary - ' + str(key) + ' = ' + str(dict[key]))
-	except:
-		logger.info('AWS Dynamic Prefix List Lambda - Error - logDictionary - ')
+	except Exception as error:
+		logger.info('AWS Dynamic Prefix List Lambda - Error - logDictionary - ' + error)
 
 def logList(l):
 	for v in l:
-		logger.info('AWS Dynamic Prefix List Lambda - Info - logList - ' + str(l) )
+		logger.info('AWS Dynamic Prefix List Lambda - Info - logList - ' + str(v) )
 
 def getDebug():
 	try:
@@ -32,8 +32,8 @@ def getRegion():
 	try:
 		r = os.environ['AWS_REGION']
 		return r
-	except:
-		logger.info('AWS Dynamic Prefix List Lambda - Error - Could not retreive environment variable AWS_REGION')
+	except Exception as error:
+		logger.info('AWS Dynamic Prefix List Lambda - Error - getRegion - Could not retreive environment variable AWS_REGION - ' + error )
 		return 'error'
 
 def getPrefixConfig():
@@ -42,12 +42,13 @@ def getPrefixConfig():
 		prefix = os.environ['prefix']
 		d = dict(x.split("=") for x in prefix.split(";"))
 		return d
-	except:
+	except Exception as error:
+		logger.info('AWS Dynamic Prefix Lambda - Error - getPrefixConfig - ' + error)
 		return None
 
 def getURL(url):
 	try:
-		if getDebug(): logger.info('AWS Dynamic Prefix Lambda - getURL Debug - getURL(' + url + ')')
+		if getDebug(): logger.info('AWS Dynamic Prefix Lambda - Debug - getURL(' + url + ')')
 		r = requests.get(url)
 		data = r.text
 		ips = data.splitlines()
@@ -56,17 +57,15 @@ def getURL(url):
 				i = ipaddress.ip_network(ip)
 				if getDebug(): logger.info('AWS Dynamic Prefix Lambda - getURL Debug - IP = ' + ip ) 
 			except ValueError as error:
-				logger.info('AWS Dynamic Prefix Lambda - getURL Error - Line does not contain an IP ' + ip)
+				logger.info('AWS Dynamic Prefix Lambda - Error - getURL - Line does not contain an IP ' + ip)
 				ips.remove(ip)
 			except Exception as error:
-				logger.info('AWS Dynamic Prefix Lambda - getURL Error - Line does not contain an IP ' + ip)
+				logger.info('AWS Dynamic Prefix Lambda - Error - getURL - Line does not contain an IP ' + ip)
 				ips.remove(ip)
 		return set(ips)
-	except:
-		print(inst)
-		logger.info('AWS Dynamic Prefix Lambda - getURL Error - Could not read URL = ' + url + ' error = ' + error)
+	except Exception as error:
+		logger.info('AWS Dynamic Prefix Lambda - Error - getURL - Could not read URL = ' + url + ' error = ' + error)
 		return None
-
 
 def prefixlist_exists(client, name):
 	try:
@@ -82,8 +81,8 @@ def prefixlist_exists(client, name):
 		)
 		logger.info('AWS Dynamic Prefix Lambda - prefixlist_exists TEST - ' + response['PrefixLists'][0])
 		# need to check if response is valid and return true or false -----------------------------------------------------------
-	except:
-		logger.info('AWS Dynamic Prefix Lambda - prefixlist_exists Error - error')
+	except Exception as error:
+		logger.info('AWS Dynamic Prefix Lambda - prefixlist_exists Error - error - ' + error)
 
 def compare(xset, yset):
 	yset = set(yset)
@@ -98,16 +97,14 @@ def lambda_handler(event, context):
 		session     = boto3.Session(region_name=region)
 		ec2         = session.client('ec2')
 		prefixlists = getPrefixConfig()
+		if getDebug(): logger.info('AWS Dynamic Prefix Lambda - Debug - information - region=' + str(region))
 		if getDebug(): logDictionary(prefixlists)
 		for prefixlist_key in prefixlists:
 			prefixlist_value = prefixlists[prefixlist_key]
 			prefixlist_cidrs = getURL(prefixlist_value)
+			if getDebug(): logger.info('AWS Dynamic Prefix Lambda - Debug - prefixlist_key: ' + str(prefixlist_key))
 			if getDebug(): logDictionary(prefixlist_cidrs)
-			# if getDebug(): logger.info('AWS Dynamic Prefix Lambda - Debug - prefixlist_key: ' + ''.join(map(str,prefixlist_key)))
-			# if getDebug(): logger.info('AWS Dynamic Prefix Lambda - Debug - prefixlist_key: ' + prefixlists[prefixlist_key])
-			# if getDebug(): logger.info('AWS Dynamic Prefix Lambda - Debug - prefixlist_value: ' + prefixlist_value)
-			# if getDebug(): logger.info('AWS Dynamic Prefix Lambda - Debug - prefixlist_cidrs: ' + prefixlist_cidrs)
-			# prefixlist_exists(ec2, prefixlist_key)
+			# if prefixlist_exists(ec2, prefixlist_key)
 	except Exception as e:
 		logger.info('AWS Dynamic Prefix Lambda - Error ' + traceback.format_exc())
 		logger.info(e)
