131a7b1e4c391cd010426e9bd1075488bceda2af
Stuck <StuckF@dnb.com>
2021-09-08 09:25:25 -0400

    init 3

--- awsdynprefix/awsdynprefix.py
+++ awsdynprefix/awsdynprefix.py
@@ -208,8 +208,8 @@ def lambda_handler(event, context):
 			if getDebug(): logger.info('AWS Dynamic Prefix Lambda - Debug - prefixlist_key: ' + str(prefixlist_key))
 			if getDebug(): logList(prefixlist_cidrs)
 			if prefixlist_exists(ec2, prefixlist_key):
-				if getDebug(): logger.info('AWS Dynamic Prefix Lambda - Debug - prefix list exists update')
-				update_prefixlist(ec2, prefixlist_key, prefixlist_cidrs)
+				# if getDebug(): logger.info('AWS Dynamic Prefix Lambda - Debug - prefix list exists update')
+				# update_prefixlist(ec2, prefixlist_key, prefixlist_cidrs)
 				if getDebug(): logger.info('AWS Dynamic Prefix Lambda - TEST - update with test_cidrs2')
 				update_prefixlist(ec2, prefixlist_key, test_cidrs2)
 				if getDebug(): logger.info('AWS Dynamic Prefix Lambda - TEST - update with test_cidrs1')
